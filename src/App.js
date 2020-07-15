28fe0b2e0e53410fd8688e3c200154175a65d548
piya03 <piyachaudhary50@gmail.com>
2020-07-15 23:36:23 +0530

    first commit UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="">
+        <button>Open Google Map</button>
+      </div>
+      <div className="container">
+        <div>
+          <input
+            type="text"
+            className="searchBox"
+            placeholder="Search Your Location"
+          />
+          <span className="icon">
+            <i class="fa fa-search"></i>
+          </span>
+        </div>
+
+        <div className="align_Map_address">
+          <div className="map_box">Here Google MAP</div>
+          <div className="address_inputs">
+            <input
+              type="text"
+              placeholder="Current Location"
+              className="current_location"
+            />
+            <input type="text" placeholder="Pincode" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
