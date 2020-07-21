0c6946bb95d6ccc9b33cc02234ad2117f1b46e5f
piya03 <piyachaudhary50@gmail.com>
2020-07-21 23:28:06 +0530

    make address option from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import {
   withScriptjs,
@@ -32,8 +32,33 @@ const MyMapComponent = compose(
 ));
 
 ///////
-
 function App() {
+  const [addressOption, setAddressOption] = useState([]);
+  console.log("App -> addressOption", addressOption);
+
+  async function onChange(e) {
+    const { value } = e.target;
+    const url = `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_API_KEY}&q=${value}`;
+
+    if (value.length > 3) {
+      const res = await fetch(url).then((res) => res.json());
+      // res.map((each) => {
+      //   console.log("onChange -> each", each.address.name);
+      //   setAddressOption.push(...addressOption, each.address.name);
+      // });
+      setAddressOption(res);
+      console.log(res);
+    }
+  }
+
+  const optionSelect = addressOption.map((each) => {
+    return {
+      label: each?.display_name || "",
+      value: each?.place_id,
+      ...each,
+    };
+  });
+  console.log("App -> optionSelect", optionSelect);
   return (
     <div className="App">
       <div className="">
@@ -45,6 +70,7 @@ function App() {
             type="text"
             className="searchBox"
             placeholder="Search Your Location"
+            onChange={onChange}
           />
           <span className="icon">
             <i className="fa fa-search"></i>
@@ -70,3 +96,53 @@ function App() {
 }
 
 export default App;
+
+/**
+ *   <PlacesAutocomplete
+            value={inputAddress.address}
+            onChange={handleChange}
+            onSelect={handleSelect}
+          >
+            {({
+              getInputProps,
+              suggestions,
+
+              getSuggestionItemProps,
+              loading,
+            }) => {
+              console.log("App -> suggestions", suggestions);
+              return (
+                <div>
+                  <input
+                    {...getInputProps({
+                      placeholder: "Search Places ...",
+                      className: "location-search-input",
+                    })}
+                  />
+                  <div className="autocomplete-dropdown-container">
+                    {loading && <div>Loading...</div>}
+                    {suggestions.map((suggestion) => {
+                      const className = suggestion.active
+                        ? "suggestion-item--active"
+                        : "suggestion-item";
+                      // inline style for demonstration purpose
+                      const style = suggestion.active
+                        ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                        : { backgroundColor: "#ffffff", cursor: "pointer" };
+                      return (
+                        <div
+                          {...getSuggestionItemProps(suggestion, {
+                            className,
+                            style,
+                          })}
+                        >
+                          <span>{suggestion.description}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            }}
+          </PlacesAutocomplete>
+ */
