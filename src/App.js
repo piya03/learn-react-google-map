610d450cc35288b6b1bfcce7480e2a42c61895d7
piya03 <piyachaudhary50@gmail.com>
2020-07-24 23:46:30 +0530

    learning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import {
   withScriptjs,
@@ -15,30 +15,38 @@ import PlacesAutocomplete, {
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAP_KEY}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
   withGoogleMap
-)((props) => (
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: -34.397, lng: 150.644 }} />
-    )}
-  </GoogleMap>
-));
+)((props) => {
+  console.log("props", props);
+  const ref = useRef(null);
+  React.useEffect(() => {
+    props.myRef.current = ref.current;
+  }, [ref.current]);
+  return (
+    <GoogleMap
+      ref={ref}
+      defaultZoom={8}
+      defaultCenter={props.latLng || { lat: -34.397, lng: 150.644 }}
+    >
+      {props.isMarkerShown && <Marker position={props.latLng} />}
+    </GoogleMap>
+  );
+});
 
 ///////
 function App() {
   const [addressOption, setAddressOption] = useState([]);
+  const [selectedValue, setSelectedValue] = useState(null);
   const [inputVal, setInputVal] = useState("");
   const [pincode, setPincode] = useState();
   const [show, setShow] = useState(true);
-  const [latitude, setLatitude] = useState();
-  const [longitude, setLongitude] = useState();
+  const myRef = useRef(null);
 
   async function onChange(e) {
     const { value } = e.target;
@@ -51,17 +59,19 @@ function App() {
     }
   }
 
-  const optionSelect = addressOption?.map((each) => {
+  useEffect(() => {
+    console.log("ref", myRef.current);
+
+    // ref.current.setCenter("");
+  }, []);
+  const optionSelect = (addressOption || [])?.map((each) => {
     return {
       label: each?.display_name || "",
       value: each?.place_id,
       ...each,
     };
   });
-
-  // const save = optionSelect.map((each, i) => {
-  //   return setLatitude(each?.lat), setLongitude(each?.lon);
-  // });
+  console.log("App -> myRef", myRef);
   console.log("App -> optionSelect", optionSelect);
   return (
     <div className="App">
@@ -82,18 +92,28 @@ function App() {
             <i className="fa fa-search"></i>
           </span>
         </div>
-
-        {/* /////////// */}
         {show && (
-          <ul
-            onClick={(e) => {
-              setShow(false);
-              setInputVal(e.target.innerText);
-            }}
-          >
+          <ul>
             {optionSelect.map((each, i) => {
               return (
-                <li key={i} value={each.value}>
+                <li
+                  onClick={(e) => {
+                    setShow(false);
+                    setSelectedValue(each);
+                    setInputVal(each?.display_name);
+                    // if (each) {
+                    //   console.log("App -> selectedValue", selectedValue);
+                    //   myRef.current.setCenter(
+                    //     window.google.maps.LatLng(
+                    //       parseInt(each?.lat),
+                    //       parseInt(each?.lon)
+                    //     )
+                    //   );
+                    // }
+                  }}
+                  key={i}
+                  value={each.value}
+                >
                   {each.label}
                 </li>
               );
@@ -104,9 +124,16 @@ function App() {
         <div className="align_Map_address">
           <div className="map_box">
             <MyMapComponent
+              myRef={myRef}
               isMarkerShown
-              latitude={latitude}
-              longitude={longitude}
+              latLng={
+                selectedValue
+                  ? {
+                      lat: parseInt(selectedValue?.lat),
+                      lng: parseInt(selectedValue?.lon),
+                    }
+                  : null
+              }
             />
           </div>
           <div className="address_inputs">
